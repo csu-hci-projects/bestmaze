c51b027d6fa86479e922f0ff5b1c7661dbfd971a
sidsutton <sisutton@rams.colostate.edu>
2020-04-10 19:38:16 -0600

    colored platform

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -15,6 +15,8 @@ public class Spawner : MonoBehaviour
     public Vector3 platformSize; 
     public Vector3 playerStart;
     public MazeGen mazeGen;
+    public int perspective;
+    public List<Material> colors;
     
 
     void Start()
@@ -22,32 +24,52 @@ public class Spawner : MonoBehaviour
         
         ArrayList mazeRaw = mazeGen.create((int)worldSize);
 
+        perspective = 1;    //egocentric: 0, allocentric: 1
+
         float a = (worldSize+1)/2;
-        platformSize = new Vector3(worldSize, 0.5f, worldSize);
-        playerStart = new Vector3(1f, 1, 1f);
+        //platformSize = new Vector3(worldSize, 0.5f, worldSize);
+        playerStart = new Vector3(1f, 0.5f, 1f);
         //Instantiates the predefined platform prefab at the (0,0,0) position:
-        GameObject pf = Instantiate(platform, new Vector3(a,0,a), Quaternion.identity);
+        //GameObject pf = Instantiate(platform, new Vector3(a,0,a), Quaternion.identity);
         //sets the prefab of the platform to a desired size:
-        pf.transform.localScale = platformSize;
+        //pf.transform.localScale = platformSize;
+        SpawnPlatform(worldSize, mazeRaw);
         SpawnPlayer();
         SpawnFences(mazeRaw);
+    }
+
+    void SpawnPlatform(float worldSize, ArrayList maze)
+    {
+        for(int i = 0; i < worldSize; i++)
+        {
+            Material strip = colors[Random.Range(0, colors.Count)];
+            for (int j = 0; j < worldSize; j++)
+            {
+                GameObject floor = Instantiate(platform, new Vector3(i, 0, j), Quaternion.identity);
+                floor.GetComponent<MeshRenderer>().material = strip;
+            }
+        }
     }
 
     void SpawnPlayer()
     {   
         GameObject playr = Instantiate(player, playerStart, Quaternion.identity);
-        GameObject lighting = Instantiate(light, playerStart, Quaternion.identity);
+        GameObject lighting = Instantiate(light, new Vector3(1f, 0.5f, 1f), Quaternion.identity);
         lighting.transform.SetParent(playr.transform);
 
         //first-person
-        GameObject camera = Instantiate(cam, new Vector3(playerStart.x, playerStart.y, playerStart.z), Quaternion.identity);        //first-person, egocentric
-        camera.transform.SetParent(playr.transform);                                                                              //first-person, egocentric
-
+        if (perspective == 0)
+        {
+            GameObject camera = Instantiate(cam, new Vector3(playerStart.x, playerStart.y, playerStart.z), Quaternion.identity);        //first-person, egocentric
+            camera.transform.SetParent(playr.transform);                                                                                //first-person, egocentric
+        }
 
         //third-person
-        //GameObject camera = Instantiate(cam, new Vector3(worldSize/2, worldSize, worldSize/2), Quaternion.identity);                                               //third-person, allocentric
-        //camera.transform.rotation = Quaternion.Euler(90,0,0);                                                                           //third-person, allocentric
-        
+        if (perspective == 1)
+        {
+            GameObject camera = Instantiate(cam, new Vector3(worldSize / 2, worldSize, worldSize / 2), Quaternion.identity);            //third-person, allocentric
+            camera.transform.rotation = Quaternion.Euler(90, 0, 0);                                                                     //third-person, allocentric
+        }
         
     }
 
@@ -64,29 +86,5 @@ public class Spawner : MonoBehaviour
                 }
             }
         }
-        //for (int i = 0; i < worldSize; i++)
-        //{
-        //    Instantiate(fence, new Vector3(0, 0, i), Quaternion.identity);
-        //    Instantiate(fence, new Vector3(i, 0, 0), Quaternion.identity);
-        //    Instantiate(fence, new Vector3(worldSize, 0, i), Quaternion.identity);
-        //    Instantiate(fence, new Vector3(i, 0, worldSize), Quaternion.identity);
-        //}
-        //Instantiate(fence, new Vector3(worldSize, 0, worldSize), Quaternion.identity); //fixes that one random border piece that's missing
-        //for (int i = 0; i < worldSize; i++)
-        //{
-        //    for (int j = 1; j < worldSize; j++)
-        //    {
-        //        if (i % 4 == 0)
-        //        {
-        //            Instantiate(fence, new Vector3(i, 0, j + 1), Quaternion.identity);
-        //        }
-        //        else if (i % 2 == 0)
-        //        {
-        //            Instantiate(fence, new Vector3(i, 0, j - 1), Quaternion.identity);
-        //        }
-        //    }
-        //}
-
-
     }
 }
